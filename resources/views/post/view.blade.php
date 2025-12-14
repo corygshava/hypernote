29b3e150296304a109cdecf32bf5fc9ac0b79108
corygshava <corygshava@gmail.com>
2025-12-14 15:22:08 +0300

    fixed the postdisplay textarea

--- resources/views/post/view.blade.php
+++ resources/views/post/view.blade.php
@@ -57,7 +57,7 @@
 							{!! $mybody !!}
 						</div>
 					@else
-						<textarea data-subrole="mybody" id="modalmessage" cols="30" rows="10" class="postDisplay">{{ $mybody }}</textarea>
+						<textarea data-subrole="mybody" id="modalmessage" cols="30" rows="10" class="postDisplay" readonly>{{ $mybody }}</textarea>
 					@endif
 				</div>
 
